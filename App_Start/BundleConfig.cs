212509a4a143f5b04a207f3fb78369d616939e92
TriptSharma <tript35.sharma@gmail.com>
2020-08-17 01:11:49 +0530

    Modify CSS and add Navbar

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace FoodErp
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-superhero.css",
                       "~/Content/site.css"));
         }
     }
