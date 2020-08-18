65d73d10bf52ffbcb96d6db389c96b3379aa9b6e
TriptSharma <tript35.sharma@gmail.com>
2020-08-18 20:58:29 +0530

    Add REST-API consumption (Delete, Update)

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -47,13 +47,60 @@ namespace FoodErp.Controllers
             {
                 return RedirectToAction("Index");
             }
-            else
-            {
-                ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
-
-            }
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
 
             return View(store);
         }
+
+        public ActionResult Update(int id)
+        {
+            StoreViewModel storeInDb = new StoreViewModel();
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:44345/api/");
+
+            var getTask = httpClient.GetAsync("stores?id=" + id.ToString());
+            getTask.Wait();
+
+            var readData = getTask.Result;
+            if (readData.IsSuccessStatusCode)
+            {
+                var dbData = readData.Content.ReadAsAsync<StoreViewModel>();
+                dbData.Wait();
+                storeInDb = dbData.Result;
+            }
+            return View(storeInDb);
+        }
+
+        [HttpPost]
+        public ActionResult Update(StoreViewModel store)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:44345/api/");
+
+            var putTask = httpClient.PutAsJsonAsync<StoreViewModel>("stores", store);
+                putTask.Wait();
+
+            var putResult = putTask.Result;
+            if (putResult.IsSuccessStatusCode)
+                return RedirectToAction("Index");
+
+            return View(store);
+        }
+            //HTTP POST
+
+        public ActionResult Delete(int id) {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:44345/api/");
+
+            var deleteTask = httpClient.DeleteAsync("stores/" + id.ToString());
+            deleteTask.Wait();
+
+            var result = deleteTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+        return RedirectToAction("Index");
+    }
     }
 }
