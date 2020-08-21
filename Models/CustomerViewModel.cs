4261fd9043b6d176372a33ff9fd3fe79af5fbc4c
TriptSharma <tript35.sharma@gmail.com>
2020-08-21 17:12:14 +0530

    Add Customer ViewModel

---
+++ Models/CustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodErp.Models
+{
+    public class CustomerViewModel
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public float Purchase { get; set; }
+        public StoreViewModel Store { get; set; }
+    }
+}
