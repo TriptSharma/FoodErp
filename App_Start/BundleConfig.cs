27fd88949400bf78fab49747fe981ee2755a0db5
TriptSharma <tript35.sharma@gmail.com>
2020-08-28 01:44:22 +0530

    Integrate AngularJS and Read WebAPI customers

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,6 +9,7 @@ namespace FoodErp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/angular.js",
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
---
+++ Scripts/Angular/CustomerScript.js
@@ -0,0 +1,14 @@
+﻿var app = angular
+        .module("CustomerApp", [])
+        .controller("customerController", function ($scope, $http, $log) {
+            $http.get("https://localhost:44345/api/customers")
+                .then(function (response) {
+                    $scope.customers = response.data;
+                    $log.info;
+                },
+                    function (response) {
+                        $scope.error = response.data;
+                        $log.info;
+                    }
+                )
+});
