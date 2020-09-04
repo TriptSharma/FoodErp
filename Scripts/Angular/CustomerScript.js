63607fe3c46600b92837dfa73ec476179262652c
TriptSharma <tript35.sharma@gmail.com>
2020-09-04 12:53:52 +0530

    Fix Create function bug in Customers

--- Scripts/Angular/CustomerScript.js
+++ Scripts/Angular/CustomerScript.js
@@ -1,21 +1,26 @@
 ﻿app.controller("customerController", function ($scope, $http, $log) {
-    $http.get("https://localhost:44345/api/customers")
-        .then(function (response) {
-            $scope.customers = response.data;
-            $log.info;
-        }, function (response) {
+
+    $scope.getCustomer = function () {
+
+        $http.get("https://localhost:44345/api/customers")
+            .then(function (response) {
+                $scope.customers = response.data;
+                $log.info;
+            }, function (response) {
                 $scope.error = response.data;
                 $log.info;
             }
-        );
+            );
+
+    }
 
     $scope.create = function () {
         $http({
             method: "POST",
             url: "https://localhost:44345/api/customers",
             data: $scope.newCustomer
-            }).success(function (response) {
-                $scope.newCustomer = null;
+        }).success(function (response) {
+            $scope.getCustomer();                
                 $log.info;
             }).error(function(response) {
                 $scope.error = response.data;
@@ -27,10 +32,13 @@
     $scope.delete = function (id) {
         $http.delete("https://localhost:44345/api/customers/" + id)
             .success(function (response) {
+                $scope.getCustomer();
                 $log.info;
             }).error(function (response) {
                 $log.info;
             })
     };
+
+    $scope.getCustomer();
 
 });
